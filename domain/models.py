58cee5d68b6f1065ee508d2ef96f1e71391c86bb
femarko <fenstermark7@gmail.com>
2025-03-01 22:56:16 +0100

    Project structure and domain/models.py added.

---
+++ domain/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import Optional, TypeVar
+
+
+class Base:
+    pass
+
+
+class User(Base):
+    def __init__(
+            self,
+            name: str,
+            email: str,
+            password: str,
+            user_id: Optional[int] = None,
+            creation_date: Optional[datetime] = None
+    ):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.creation_date = creation_date
+
+
+ModelClass = TypeVar("ModelClass", bound=Base)
